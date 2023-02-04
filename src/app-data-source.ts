794739a9ee4aac3467a9a68ed7b6b6798015ca7c
nikitamakovey <nikitamakovey@yahoo.com>
2023-02-04 17:40:52 +0800

    Add app data source to fetch repositories

---
+++ src/app-data-source.ts
@@ -0,0 +1,21 @@
+import { DataSource } from "typeorm"
+import entities from "./config/typeorm/entities";
+
+export const AppDataSource = new DataSource({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: entities,
+    synchronize: true,
+})
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    });
